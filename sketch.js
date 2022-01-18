d43e465f1f2302a452624153245cc01a42da9f67
kieranthomson100 <97965115+kieranthomson100@users.noreply.github.com>
2022-01-18 14:13:41 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  rectMode(CENTER)
+  createCanvas(1080, 1920);
+  //background(0);
+}
+
+function draw() {
+  background(0);
+  rect(random(width),random(height),random(width),random(height))
+  fill(0)
+
+  rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(0)
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(0)
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(0)
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  rect(random(width),random(height),random(width),random(height))
+  fill(0)
+   rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+rect(random(width),random(height),random(width),random(height))
+  fill(255,0,0)
+  
+  
+  noStroke()
+  
+}
